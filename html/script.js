aea7bc2200e6df3dc3ea977acb4d752dd05ae073
jadeola <jadeola@unomaha.edu>
2016-09-23 21:06:06 -0500

    Modified Pages
    Added Script file & modified the pages to better suite the mockups.

---
+++ html/script.js
@@ -0,0 +1,11 @@
+
+$(function(){
+  // bind change event to select
+  $('#parkingSelect').on('change', function () {
+      var url = $(this).val(); // get selected value
+      if (url) { // require a URL
+          window.location = url; // redirect
+      }
+      return false;
+  });
+});
