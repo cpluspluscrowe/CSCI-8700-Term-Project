c8aa222bd48bc9fd7704da32924ad47549e0886f
vpadam <vpadam@unomaha.edu>
2016-10-15 14:43:57 -0500

    vpadam -> adding DriverRefund.php

---
+++ Template/php/DriverRefund.php
@@ -0,0 +1,90 @@
+<html>
+    <?php 
+include "header.php";
+?> 
+	<head>
+		
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+               
+                <link rel="stylesheet" type="text/css" href="../css/main.css">
+	</head>
+	<body class="subpage">
+
+
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="inner">
+					<header class="align-center">
+						<h1>Drivers' Refund Page</h1>
+						<p>List of Drivers with Refund Requests </p>
+					</header>
+
+				</div>
+
+									<div id="content">
+										<table style="center">
+											<thead style="font-size: 20px; color: blue">
+												<tr>
+													<th>S.No</th>
+                                                                                                        <th>        </th>
+													<th>Name</th>
+                                                                                                        <th>        </th>
+													<th>Request-Time</th>
+                                                                                                        <th>        </th>
+													<th>Rating</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>1</td>
+                                                                                                        <th>        </th>
+													<td>Matthew</td>
+                                                                                                        <th>        </th>
+													<td>16:45</td>
+                                                                                                        <th>        </th>
+													<td><a href="#" class="button special">Rate</td>
+												</tr>
+												<tr>
+													<td>2</td>
+                                                                                                        <th>        </th>
+													<td>John</td>
+                                                                                                        <th>        </th>
+													<td>12:30</td>
+                                                                                                        <th>        </th>
+													<td><a href="#" class="button special">Rate</td>
+												</tr>
+												<tr>
+													<td>3</td>
+                                                                                                        <th>        </th>
+													<td>James</td>
+                                                                                                        <th>        </th>
+													<td>10.06</td>
+                                                                                                        <th>        </th>
+													<td><a href="#" class="button special">Rate</td>
+												</tr>
+												<tr>
+													<td>4</td>
+                                                                                                        <th>        </th>
+													<td>Andrew</td>
+                                                                                                        <th>        </th>
+													<td>9:17</td>
+                                                                                                        <th>        </th>
+													<td><a href="#" class="button special">Rate</td>
+												</tr>
+												<tr>
+													<td>5</td>
+                                                                                                        <th>        </th>
+													<td>Peter</td>
+                                                                                                        <th>        </th>
+													<td>9:10</td>
+                                                                                                        <th>        </th>
+													<td><a href="#" class="button special">Rate</td>
+												</tr>
+											</tbody>
+									
+										</table>
+									</div>
+
+	</body>
+</html>
