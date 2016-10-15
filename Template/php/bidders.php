19135f1bd7f4d04050647db71c47d37ed799d1c0
ImDmj <mdhaninkula@unomaha.edu>
2016-10-15 12:50:23 -0500

    Added Manju's files

---
+++ Template/php/bidders.php
@@ -0,0 +1,361 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title>MavPark</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="css/themify-icons.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="css/flexslider.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="css/ytplayer.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+    <div class="nav-container">
+
+        <nav>
+
+       	    <nav class="absolute ">
+
+		        <div class="nav-bar">
+
+		            <div class="module left">
+
+		                <a href="index.html">
+
+                            <h2 class="mb8" style="color:#e74c3c; font-weight:bold;">MavPark</h2>
+
+                        </a>
+
+		            </div>
+
+		            <div class="module widget-handle mobile-toggle right visible-sm visible-xs">
+
+		                <i class="ti-menu"></i>
+
+		            </div>
+
+		            <div class="module-group right">
+
+		                <div class="module left">
+
+		                    <ul class="menu">
+
+		                        <li>
+
+		                            <a href="index.html">Home</a>
+
+		                        </li><li>
+
+		                            <a href="about.html">about us </a>
+
+		                        </li>
+
+		                        <li class="has-dropdown">
+
+		                            <a href="#">Services </a>
+
+		                            <ul class="mega-menu">
+
+		                                <li>
+
+		                                    <ul>
+
+		                                        <li>
+
+		                                            <a href="service1.html">abc</a>
+
+		                                        </li>
+
+		                                        <li>
+
+		                                            <a href="service2.html">efg</a>
+
+		                                        </li>
+
+    	                                        <li>
+
+		                                            <a href="service3.html">hik</a>
+
+		                                        </li>
+
+		                                    </ul>
+
+		                                </li>
+
+		                            </ul>
+                                <li>
+		                            <a href="contact.html">Contact us</a>
+
+		                        </li>
+
+		                    </ul>
+
+		                </div>
+
+		            </div>
+
+                        </div>
+
+		    </nav>
+
+	 </nav>
+
+    </div>
+
+    <div class="main-container" background-image: i2.png;>
+
+        <section class="page-title page-title-4 image-bg overlay parallax">
+
+            <div class="background-image-holder">
+
+                <img alt="Background Image" class="background-image" src="img/cover14.jpg">
+
+            </div>
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-6">
+
+                        <h3 class="uppercase mb0">Bidders for Spot</h3>
+
+                    </div>
+
+                    <div class="col-md-6 text-right">
+
+                        <ol class="breadcrumb breadcrumb-2">
+
+                            <li>
+
+                                <a href="#">Home</a>
+
+                            </li>
+
+                            <li class="active">Drivers page&nbsp;</li>
+
+                        </ol>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+         <section style="padding-bottom:0px; overflow:scroll;" class="image-bg overlay">
+
+            <div class="background-image-holder">
+
+                <img alt="image" class="background-image" src="img/backround.jpg"sss>
+
+            </div>
+
+             <div class="container" style="padding:10px; background: rgba(68, 62, 62, 0.5); margin-bottom:50px; color:white;">
+
+                <div class="row">
+
+                    <div class="col-sm-10 col-sm-offset-1 ">
+
+                        <h4 class="color-primary  mb-xs-24 ">MavPark</h4>
+
+                        </div>
+
+                    </div>
+
+                    <div class="row mb32 mb-xs-16">
+
+                        <div class="col-sm-10 col-sm-offset-1 ">
+
+                            <p class="lead"style="text-align:justify";letter-spacing: 2px;>
+
+                                   <table>
+
+                                    <tbody>
+
+                                        <tr>
+
+                                            <td width="25%"><strong>Drivers names</strong></td>
+
+                                            <td width="25%"><strong>ratings</strong></td>
+
+                                            <td width="25%"><strong>Bids</strong></td>
+
+                                        </tr>
+
+                                        <tr>
+
+                                            <td><strong>Riddhi</strong></td>
+
+                                            <td>4/5</td>
+
+                                            <td>$1</td>
+
+                                        </tr>
+
+                                        <tr>
+
+                                            <td><strong>Vaishnavi</strong></td>
+
+                                            <td>4.2/5</td>
+
+                                            <td>$2</td>
+
+                                        </tr>
+
+                                        <tr>
+
+                                            <td><strong>james</strong></td>
+
+                                            <td>4.4/5</td>
+
+                                            <td>$3</td>
+
+                                        </tr>
+
+                                    </tbody>
+
+                                </table></centre>
+
+                            </p>
+
+							</div>
+
+		<div class="nav-container">
+
+		</div>		
+
+		<div class="main-container">
+
+		<section>
+
+		        <div class="container">
+
+		            <div class="row v-align-children">
+
+                        <div class="col-sm-4 text-center">
+
+		                    <div class="feature boxed cast-shadow-light">
+
+		                        <img alt="Pic" class="image-small inline-block mb24" src="img/avatar2.png">
+
+		                        <h4>James</h4>
+
+                                <span>
+
+		                            <strong>car model</strong> toyota</span>
+
+		                        <p>
+
+		                            review of driver
+
+		                        </p>
+
+		                        <span>
+
+		                            <strong>Contact</strong> 12345</span>
+
+		                    </div>
+
+		                <!--<div class="col-md-6 col-md-push-3 text-center">
+
+                        <!--<div class="col-md-4 col-md-offset-1 col-sm-5 col-sm-offset-1">-->
+
+		                    <a class="btn-filled btn" href="#">accept</a>
+
+                            <a class="btn-filled btn" href="#">reject</a>
+
+		                </div>
+
+		            <div class="col-md-7 col-sm-6 text-center mb-xs-10">
+
+		                    <img class="cast-shadow" alt="Screenshot" src="img/Bugatti-Chiron-2016.jpg">
+
+		                </div>--></div>
+
+		        </div>
+		       
+		    </section></div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+<footer class="footer-1 bg-dark">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-4 col-sm-6">
+
+                    </div>
+
+                </div>
+
+                <div class="row">
+
+                    <div class="col-sm-6">
+
+                        <span class="sub" t>© Copyright 2016 RockUrSocksOff</span>
+
+                    </div>
+
+                    <div class="col-sm-6 text-right">
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <a class="btn btn-sm fade-half back-to-top inner-link" href="#top">Top</a>
+
+        </footer>
+
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/twitterfetcher.min.js"></script>
+
+    <script src="js/spectragram.min.js"></script>
+
+    <script src="js/smooth-scroll.min.js"></script>
+
+    <script src="js/parallax.js"></script>
+
+    <script src="js/scripts.js"></script>
+
+</body>
+
+</html>
