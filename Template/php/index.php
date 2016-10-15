88587692f6514de92682e43b15a02c8359c67848
rmayekar <rmayekar@unomaha.edu>
2016-10-15 15:38:57 -0500

    modifying index.php

---
+++ Template/php/index.php
@@ -0,0 +1,310 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title>MavPark</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="../css/themify-icons.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="../css/flexslider.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="../css/lightbox.min.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="../css/ytplayer.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="../css/theme.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href="../css/custom.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="../css/mav.css" rel="stylesheet" type="text/css" media="all" />
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400%7CRaleway:100,400,300,500,600,700%7COpen+Sans:400,500,600' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+    <div class="nav-container">
+
+        <nav>
+
+       	    <nav class="absolute ">
+
+		        <div class="nav-bar">
+
+		            <div class="module left">
+
+		                <a href="index.html">
+
+                            <h2 class="mb8" style="color:#e74c3c; font-weight:bold;">MavPark</h2>
+
+                        </a>
+
+		            </div>
+
+		            <div class="module widget-handle mobile-toggle right visible-sm visible-xs">
+
+		                <i class="ti-menu"></i>
+
+		            </div>
+
+		            <div class="module-group right">
+
+		                <div class="module left">
+
+		                    <ul class="menu">
+
+		                        <li>
+
+		                            <a href="index.html">Home</a>
+
+		                        </li><li>
+
+		                            <a href="about.html">about us </a>
+
+		                        </li>
+
+		                        <li class="has-dropdown">
+
+		                            <a href="#">Services </a>
+
+		                            <ul class="mega-menu">
+
+		                                <li>
+
+		                                    <ul>
+
+		                                        <li>
+
+		                                            <a href="service1.html">abc</a>
+
+		                                        </li>
+
+		                                        <li>
+
+		                                            <a href="service2.html">efg</a>
+
+		                                        </li>
+
+    	                                        <li>
+
+		                                            <a href="service3.html">hik</a>
+
+		                                        </li>
+
+		                                    </ul>
+
+		                                </li>
+
+		                            </ul>
+
+                                <li>
+
+		                            <a href="contact.html">Contact us</a>
+
+		                        </li>
+
+		                    </ul>
+
+		                </div>
+
+		            </div>
+
+		        </div>
+
+		    </nav>
+
+	 </nav>
+
+    </div>
+
+    <div class="main-container" background-image: i2.png;>
+
+        <section class="page-title page-title-4 image-bg overlay parallax">
+
+            <div class="background-image-holder">
+
+                <img alt="Background Image" class="background-image" src="../images/cover14.png">
+
+            </div>
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-6">
+
+                        <h3 class="uppercase mb0">Home</h3>
+
+                    </div>
+
+                    <div class="col-md-6 text-right">
+
+                        <ol class="breadcrumb breadcrumb-2">
+
+                            <li>
+
+                                <a href="#">Home</a>
+
+                            </li>
+
+                            <li class="active">Drivers page&nbsp;</li>
+
+                        </ol>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+         <section style="padding-bottom:0px; overflow:scroll;" class="image-bg overlay">
+
+            <div class="background-image-holder">
+
+                <img alt="image" class="background-image" src="../images/backround.jpg">
+
+            </div>
+
+             <div class="container" style="padding:10px; background: rgba(68, 62, 62, 0.5); margin-bottom:50px; color:white;">
+
+                <div class="row">
+
+                    <div class="col-sm-10 col-sm-offset-1 ">
+
+                        <h4 class="color-primary  mb-xs-24 ">MavPark</h4>
+
+                        </div>
+
+                    </div>
+
+                    <div class="row mb32 mb-xs-16">
+
+                        <div class="col-sm-10 col-sm-offset-1 ">
+
+                            <p class="lead"style="text-align:justify";letter-spacing: 2px;>
+
+                           
+
+                            </p>
+
+							</div>
+
+		<div class="nav-container">
+
+		</div>
+
+		<div class="main-container">
+
+		<section>
+
+		        <div class="container">
+
+		            <div class="row v-align-children">
+
+                        <div class="col-sm-4 text-center">
+
+		                    <div class="feature boxed cast-shadow-light">
+
+		                        
+
+		                        <h4>MavPark is special, because it interfaces drivers with users. Users will be able to
+
+signal that they are leaving a parking spot. This interaction will direct drivers to open
+
+parking spots. This will reduce overall congestion and the time required to find parking.
+
+Faster parking results in higher user satisfaction.</h4>
+
+                               
+
+		                    </div>
+
+
+		                <!--<div class="col-md-6 col-md-push-3 text-center">
+
+                        <!--<div class="col-md-4 col-md-offset-1 col-sm-5 col-sm-offset-1">-->
+		                    
+		                    
+
+		                </div>
+
+		            <div class="col-md-7 col-sm-6 text-center mb-xs-10">
+
+                                <a class="btn-filled btn" href="#">REGISTER</a><br>
+                                      <a class="btn-filled btn" href="#">LOGIN</a>
+
+		        </div>
+
+		    </section></div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+<footer class="footer-1 bg-dark">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-md-4 col-sm-6">
+                  
+                    </div>
+                    
+                </div>
+
+                <div class="row">
+
+                    <div class="col-sm-6">
+
+                        <span class="sub" t>© Copyright 2016 RockUrSocksOff</span>
+
+                    </div>
+
+                    <div class="col-sm-6 text-right">
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <a class="btn btn-sm fade-half back-to-top inner-link" href="#top">Top</a>
+
+        </footer>
+
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/twitterfetcher.min.js"></script>
+
+    <script src="js/spectragram.min.js"></script>
+
+    <script src="js/smooth-scroll.min.js"></script>
+
+    <script src="js/parallax.js"></script>
+
+    <script src="js/scripts.js"></script>
+
+</body>
+
+</html>
