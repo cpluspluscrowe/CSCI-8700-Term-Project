1d0921b10c03869e767b41a5409a7bfe8a03393a
vpadam <vpadam@unomaha.edu>
2016-10-15 14:45:14 -0500

    vpadam -> adding help.php

---
+++ Template/php/help.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<!--
+	Intensify by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+    <?php 
+include "header.php";
+?> 
+	<head>
+		<title>Driver Refund Page</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="stylesheet" type="text/css" href="../css/main.css">
+                
+	</head>
+	<body class="subpage">
+		<!-- Main -->
+			<section id="main" class="wrapper">
+				<div class="inner">
+					<header class="align-center">
+						<h1>Help Page</h1>
+						<p>Information page about Website</p>
+					</header>
+                                    <div id="content">
+					<h4>WHAT is it about</h4>
+	  					<p style="font-size:15px">The application is purposed to provide parking availability by giving an access to the aerial view of UNO campus parking. The driver can book the available slot by interacting with the user already parked in the spot and ready to leave. This gives an easy way to find parking spots and reduces the traffic around UNO campus. </p>
+                                        <h4>WHO can access the application</h4>
+	  					<p style="font-size:15px">All students and faculty members of UNO can access the application with their valid NUID or UNO email address. </p>
+					<h4>HOW to sign-in</h4> 
+	  					<p style="font-size:15px">Users can access the application by signing-up with their UNO-NUID and password. An account is created which is unique for each NUID.  </p>
+					<h4>WHY to use this application</h4>
+                                                <p style="font-size:15px"> Apart from having the access to the open parking spots, this application helps to save a dollar by giving your spot to someone searching for it. </p>
+					<h4>WHEN can you request a refund</h4>
+	  					
+                                                <p style="font-size:15px">if the transaction fails</p>
+        					<p style="font-size:15px">if the parking you booked is not provided</p>
+        					<p style="font-size:15px">if you booked it by accident (accidental purchase refunds)</p>
+        					<p style="font-size:15px">if somebody else is using your account without your consent (unauthorized purchase refunds)</p>
+                                    </div>	
+				</div>
+			</section>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
