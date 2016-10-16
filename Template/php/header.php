bc432e10817402e761ecf5beea0ee5fbc181a407
rmayekar <rmayekar@unomaha.edu>
2016-10-15 19:41:13 -0500

    link to header

--- Template/php/header.php
+++ Template/php/header.php
@@ -4,15 +4,20 @@
     <head>
         <meta charset="UTF-8">
         <style>
-            
+            h4{
+                padding: 0 0 0 350px;
+                font-weight: bold;
+                font-size: 10px;
+            }
             </style>
     </head>
     <link href="mav.css" rel="stylesheet" type ="text/css">
     <body>
         <ul>
-  <li><a href="http://localhost/MavPark/branches/php/home.php">Home</a></li>
+  <li><a href="http://localhost/MavPark/CSCI-8700-Term-Project/Template/php/home.php">Home</a></li>
   <li><a href="#news">About Us</a></li>
   <li><a href="#contact">Contact</a></li>
+  <li><h4>MavPark</h4> </li>
   <li style="float:right"><a class="active" href="#about">About</a></li>
 </ul>
         <?php
