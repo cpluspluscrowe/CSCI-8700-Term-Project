b91d178b58b28ce90a982e69e3554b72fc1bca50
rmayekar <rmayekar@unomaha.edu>
2016-10-15 19:49:54 -0500

    new files

---
+++ Template/php/loging.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+     <?php 
+include "header.php";
+?>   
+<head>
+    <style>
+        P{
+            padding: 200px;
+            color: red;
+            font-size: 50px;
+        }
+        </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/mav.css">
+    <!--<link rel="stylesheet" type="text/css" href="../css/mockup.css">-->
+    <script src="../script/jquery-3.1.0.js"></script>
+    <script src="../script/script.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>  
+<body> 
+<center>
+    <p>
+    <?php
+    
+    echo 'You Are Successfully Logged In';
+    ?>
+    </p>    
+</center>
+</body>
+</html>
+ <?php
+include "footer.php";
+
+?>   
