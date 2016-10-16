6dffb96e26351bb5da6a612c947432310ef052e6
rmayekar <rmayekar@unomaha.edu>
2016-10-15 19:45:00 -0500

    link to refund

--- Template/php/refund.php
+++ Template/php/refund.php
@@ -8,7 +8,8 @@ include "header.php";
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="../css/mockup.css">
+        <link rel="stylesheet" type="text/css" href="../css/mav.css">
+        <!--<link rel="stylesheet" type="text/css" href="../css/mockup.css">-->
         <script src="../script/jquery-3.1.0.js"></script>
         <script src="../script/script.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
