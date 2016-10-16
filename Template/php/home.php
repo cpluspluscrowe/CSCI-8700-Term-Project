d7c494b848921379cebbeb7a8ef8e29f3e19c0b0
rmayekar <rmayekar@unomaha.edu>
2016-10-15 19:42:31 -0500

    link to home

--- Template/php/home.php
+++ Template/php/home.php
@@ -6,7 +6,8 @@ include "header.php";
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../css/mockup.css">
+    <link rel="stylesheet" type="text/css" href="../css/mav.css">
+    <!--<link rel="stylesheet" type="text/css" href="../css/mockup.css">-->
     <script src="../script/jquery-3.1.0.js"></script>
     <script src="../script/script.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -23,9 +24,9 @@ include "header.php";
 <td></td>
 </tr>
 <tr>
-<td><a href="http://localhost/MavPark/branches/login.php"><button type="button" class="btn btn-success btn-lg">Log In</button></td>
+<td><a href="http://localhost/MavPark/CSCI-8700-Term-Project/Template/php/login.php"><button type="button" class="btn btn-success btn-lg">Log In</button></td>
 <td></td>
-<td><a href="http://localhost/MavPark/branches/register.php"><button type="button" class="btn btn-success btn-lg1">Register</button></td>
+<td><a href="http://localhost/MavPark/CSCI-8700-Term-Project/Template/php/register.php"><button type="button" class="btn btn-success btn-lg">Register</button></td>
 </tr>
 </form>
 </table>
